207b3eaf3921113357e0539d0a139da584eb26f8
Elias Ågeby <elias.ageby@echostate.se>
2025-10-31 10:26:16 +0100

    added bool literal

--- typespark/__init__.py
+++ typespark/__init__.py
@@ -3,5 +3,5 @@ from typespark.columns import TypedArrayType, TypedColumn
 from typespark.metadata import decimal, field, foreign_key, primary_key
 from typespark.struct import Struct
 
-from .literals import int_literal, string_literal
+from .literals import bool_literal, int_literal, string_literal
 from .type_alias import *
--- typespark/literals.py
+++ typespark/literals.py
@@ -1,8 +1,8 @@
 from pyspark.sql.functions import lit
-from pyspark.sql.types import IntegerType, StringType
+from pyspark.sql.types import BooleanType, IntegerType, StringType
 
 from typespark.columns import TypedColumn
-from typespark.type_alias import Integer, String
+from typespark.type_alias import Bool, Integer, String
 
 LiteralType = str | int | float | bool | None
 
@@ -17,3 +17,7 @@ def string_literal(value: str | None) -> String:
 
 def int_literal(value: int | None) -> Integer:
     return _lit(value).cast(IntegerType())
+
+
+def bool_literal(value: bool | None) -> Bool:
+    return _lit(value).cast(BooleanType())
