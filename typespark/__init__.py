175ff027bb26855a316790ba98a83d74a5207a74
Elias Ågeby <elias.ageby@echostate.se>
2026-02-10 17:22:47 +0100

    fix imports

--- typespark/__init__.py
+++ typespark/__init__.py
@@ -1,8 +1,7 @@
 from typespark._compatibility import check_pyspark_version
 from typespark.base import BaseDataFrame
-from typespark.columns import TypedArrayType, TypedColumn
+from typespark.columns import Struct, TypedArrayType, TypedColumn
 from typespark.metadata import decimal, field, foreign_key, primary_key
-from typespark.struct import Struct
 
 from .literals import bool_literal, int_literal, string_literal
 from .type_alias import *
