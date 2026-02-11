e079ada449de5704e77d1f872d3caf3ad5244963
Elias Ågeby <elias.ageby@echostate.se>
2026-02-11 15:45:37 +0100

    remove compat check

--- typespark/__init__.py
+++ typespark/__init__.py
@@ -1,9 +1,6 @@
-from typespark._compatibility import check_pyspark_version
 from typespark.base import BaseDataFrame
 from typespark.columns import Struct, TypedArrayType, TypedColumn
 from typespark.metadata import decimal, field, foreign_key, primary_key
 
 from .literals import bool_literal, int_literal, string_literal
 from .type_alias import *
-
-check_pyspark_version()
