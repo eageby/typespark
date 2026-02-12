3c07c04b9f0d917d35cac88e6f8d03b2d326d6d4
Elias Ågeby <elias.ageby@echostate.se>
2026-02-12 14:11:37 +0100

    fix struct import init

--- typespark/__init__.py
+++ typespark/__init__.py
@@ -1,5 +1,6 @@
 from typespark.base import BaseDataFrame
-from typespark.columns import Struct, TypedArrayType, TypedColumn
+from typespark.columns import TypedArrayType, TypedColumn
+from typespark.columns.struct import Struct
 from typespark.metadata import decimal, field, foreign_key, primary_key
 
 from .literals import bool_literal, int_literal, string_literal
