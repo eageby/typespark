4fa30e2e2f522fedbf7d8eb1372d0040e72e38fa
Elias Ågeby <elias.ageby@echostate.se>
2026-02-21 20:05:23 +0100

    add aggregate functions

--- typespark/functions/aggregates.py
+++ typespark/functions/aggregates.py
@@ -7,8 +7,24 @@ from typespark.type_alias import Integer
 
 
 def sum[T: DataType](col: TypedColumn[T]) -> TypedColumn[T]:
-    return _AggregateColumn(TypedColumn(F.sum(col._col)))  # type: ignore
+    return _AggregateColumn(TypedColumn(F.sum(col.to_spark())))  # type: ignore
+
+
+def first[T: DataType](col: TypedColumn[T]) -> TypedColumn[T]:
+    return _AggregateColumn(TypedColumn(F.first(col.to_spark())))  # type: ignore
+
+
+def max[T: DataType](col: TypedColumn[T]) -> TypedColumn[T]:
+    return _AggregateColumn(TypedColumn(F.max(col.to_spark())))  # type: ignore
+
+
+def max_by[T: DataType](col: TypedColumn[T], ord: TypedColumn) -> TypedColumn[T]:
+    return _AggregateColumn(TypedColumn(F.max_by(col.to_spark(), ord.to_spark())))  # type: ignore
+
+
+def min[T: DataType](col: TypedColumn[T]) -> TypedColumn[T]:
+    return _AggregateColumn(TypedColumn(F.min(col.to_spark())))  # type: ignore
 
 
 def count(col: TypedColumn) -> Integer:
-    return _AggregateColumn(TypedColumn(F.count(col._col)))  # type: ignore
+    return _AggregateColumn(TypedColumn(F.count(col.to_spark())))  # type: ignore
